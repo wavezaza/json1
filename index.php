caef731b39f3dd447c1dd47e50479bd8cc5cfed8
ReStarT <80101494+wavezaza@users.noreply.github.com>
2021-12-03 14:52:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,31 +1,58 @@
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <script src="https://code.jquery.com/jquery-3.6.0.js"
-        integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 </head>
 <body>
-    <span id="textResult"> - </span>
-    X<input type="text" id="x">
-    Y<input type="text" id="y">
-    <button id="btnAdd2" > GET </button>
-
-    
+    <input type="number" name="" id="x" />
+    <input type="number" name="" id="y" />
+    <span id="txtResult"> - </span> 
+    <button  id="get1"> GET </button>
+    <button  id="get2"> GET </button>
+    <hr>
+    <button  id="json1"> JSON1 </button>
 </body>
-
 <script>
-    $(() => {
-        $("#btnAdd2").click(() => {
+    $(()=>{
+        $("#get1").click(()=>{
             var x = $("#x").val();
             var y = $("#y").val();
-            var url="http://cj-android-demon.herokuapp.com/json1.php"
-            $.get(
-                
-                )
-            });
+            var url = "http://cj-android-demon.herokuapp.com/add.php?x=%22+x+%22&y=" + y;
+            console.log(url);
+            $.get(url, (data, status)=>{
+                console.log(data);
+                console.log(status);
+                $("#txtResult").text(data);
+            })
         });
+        $("#get2").click(()=>{
+            var x = $("#x").val();
+            var y = $("#y").val();
+            var url = "http://cj-android-demon.herokuapp.com/add2.php";
+            console.log(url);
+            $.post(url, {
+                "x": x,
+                "y": y
+            }, (data, status)=>{
+                console.log(data);
+                console.log(status);
+                $("#txtResult").text(data);
+            });
+
+        });
+        $("#json1").click(()=>{
+
+            var url = "http://cj-android-demon.herokuapp.com/json2.php";
+            console.log(url);
+            $.get(url, (data, status)=>{
+                console.log(data);
+
+            })
+        });
+    });
 </script>
 </html>

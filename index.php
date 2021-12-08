d14e0e6d25e4a180e9ea0080ef37a17e2ba8a6c7
ReStarT <80101494+wavezaza@users.noreply.github.com>
2021-12-09 00:56:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,62 +1,90 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.js"
+        integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 </head>
-    <body>
-    <input type="number" name="" id="x" />
-    <input type="number" name="" id="y" />
-    <span id="txtResult"> - </span> 
-    <button  id="get1"> GET </button>
-    <button  id="get2"> GET </button>
-    <hr>
-    <button  id="json1"> JSON1 </button>
+
+<body>
+    <button id="B_Back"> back </button>
+    <div id="Main">
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Title</th>
+                    <th>Author</th>
+                </tr>
+            </thead>
+            <tbody id="TB_Post">
+            </tbody>
+        </table>
+    </div>
+    <div id="Detail">
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Title</th>
+                    <th>UserID</th>
+                </tr>
+            </thead>
+            <tbody id="TBL_Details">
+            </tbody>
+        </table>
+    </div>
 </body>
 <script>
-    $(()=>{
-        $("#get1").click(()=>{
-            var x = $("#x").val();
-            var y = $("#y").val();
-            var url = "http://cj-android-demon.herokuapp.com/add.php?x="+x+"&y=" + y;
-            console.log(url);
-            $.get(url, (data, status)=>{
+    function showDetails(id) {
+        $("#Main").hide();
+        $("#Detail").show();
+        var url = "https://jsonplaceholder.typicode.com/posts/" + id
+        $.getJSON(url)
+            .done((data) => {
                 console.log(data);
-                console.log(status);
-                $("#txtResult").text(data);
+                var line = "<tr id='Detail_ROW'";
+                    line += "><td>" + data.id + "</td>"
+                    line += "<td><b>" + data.title + "</b><br/>"
+                    line += data.body + "</td>"
+                    line += "<td>" + data.userId + "</td>"
+                    line += "</tr>";
+                    $("#TBL_Details").append(line);
             })
-        });
-        $("#get2").click(()=>{
-            var x = $("#x").val();
-            var y = $("#y").val();
-            var url = "http://cj-android-demon.herokuapp.com/add2.php";
-            console.log(url);
-            $.post(url, {
-                "x": x,
-                "y": y
-            }, (data, status)=>{
-                console.log(data);
-                console.log(status);
-                $("#txtResult").text(data);
-            });
-        });
-        $("#json1").click(()=>{
-            var url = "https://cj-android-demon.herokuapp.com/json2.php";
-            console.log(url);
-            $.getJSON(url)
-                .done((data)=>{
-                    $.each(data, (k, item)=>{
-                        console.log(item);
-                        console.log(item.fname);
-                    });
-                })
-                .fail((shr, status, err)=>{
-                    console.log(err);
+            .fail((xhr, err, status) => {
+            })
+    }
+    function Posts() {
+        var url = "https://jsonplaceholder.typicode.com/posts"
+        $.getJSON(url)
+            .done((data) => {
+                $.each(data, (k, item) => {
+                    // console.log(item);
+                    var line = "<tr>";
+                    line += "<td>" + item.id + "</td>"
+                    line += "<td><b>" + item.title + "</b><br/>"
+                    line += item.body + "</td>"
+                    line += "<td><button onClick='showDetails(" + item.id + ");'>Link</button></td>"
+                    line += "</tr>";
+                    $("#TB_Post").append(line);
                 });
+                $("#Main").show();
+            })
+            .fail((xhr, err, status) => {
+            })
+    }
+    $(() => {
+        Posts();
+        $("#Detail").hide();
+        $("#B_Back").click(() => {
+            $("#Main").show();
+            $("#Detail").hide();
+            $("#Detail_ROW").remove();
         });
-    });
+    })
 </script>
 </html>
